da57d5a2b7ee2ffe27eadc61b58ec2c88fe63a62
Revati <revati.v.bordawekar@gmail.com>
2023-02-01 20:38:29 -0800

    updated display

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import 'mapbox-gl/dist/mapbox-gl.css'
 import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
 import React, { useState, useRef, useCallback } from 'react'
-import MapGL from 'react-map-gl'
+import Map, {NavigationControl} from 'react-map-gl'
 import Geocoder from 'react-map-gl-geocoder'
 
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_API
 
-const Example = () => {
+export default function App() {
+
   const [viewport, setViewport] = useState({
     latitude: 37.7577,
     longitude: -122.4376,
-    zoom: 8
+    zoom: 4
   });
+
+
+
   const mapRef = useRef();
   const handleViewportChange = useCallback(
     (newViewport) => setViewport(newViewport),
     []
   );
 
+ 
+
   // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
   const handleGeocoderViewportChange = useCallback(
     (newViewport) => {
-      const geocoderDefaultOverrides = { transitionDuration: 1000 };
+      const geocoderDefaultOverrides = { transitionDuration: 500 };
 
       return handleViewportChange({
         ...newViewport,
@@ -31,26 +37,40 @@ const Example = () => {
     },
     []
   );
+  const navControlStyle = {
+    left: 10,
+    bottom: 10,
+  };
 
   return (
     <div style={{ height: "100vh" }}>
-      <MapGL
+      <Map
         ref={mapRef}
         {...viewport}
         width="100%"
         height="100%"
         onViewportChange={handleViewportChange}
         mapboxApiAccessToken={MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        projection= 'globe' 
+        
       >
+       
         <Geocoder
           mapRef={mapRef}
           onViewportChange={handleGeocoderViewportChange}
           mapboxApiAccessToken={MAPBOX_TOKEN}
           position="top-left"
         />
-      </MapGL>
+      
+      <NavigationControl
+       style={navControlStyle}
+       showCompass={true}
+       showZoom={true}
+     />
+
+      </Map>
     </div>
   );
 };
 
-export default Example
